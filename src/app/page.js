10d89c81225db34b3a289bb441465ded1fe89ce1
Isa sloot <isasloot@hotmail.com>
2024-03-08 09:28:15 +0000

    dckdkj

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 import { SignUp, SignIn } from "@clerk/nextjs";
 
-const Homepage = () => {
+function Homepage() {
   return (
     <div>
       <h1>Welcome to Social media app</h1>
-      <p>Sign in or sign up to begin</p>
+      {/* <p>Sign in or sign up to begin</p>
       <div>
         <SignIn />
         <SignUp />
-      </div>
+      </div> */}
     </div>
   );
-};
+}
 export default Homepage;
